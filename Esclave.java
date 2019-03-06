a7a2c57aeb013d838f8a4dece4eeaa1b28517da3
KADRI Adlane <24720309+adlaneKadri@users.noreply.github.com>
2019-03-06 18:19:15 +0100

    Add files via upload

--- Esclave.java
+++ Esclave.java
@@ -1,3 +1,41 @@
-public class Esclave {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class Esclave implements  Runnable{
+    private final Socket socket;
+
+    public Esclave(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+
+        InputStream in = null;
+        try {
+            in = socket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int i = 0;
+        while (true){
+            try {
+                if (!((i = in.read())!= -1)) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.write(i);
+        }
+
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /* Classe Thread */
 }
--- Maitre.java
+++ Maitre.java
@@ -1,3 +1,31 @@
+import java.util.concurrent.*;
+import java.net.*;
+import java.io.*;
+
 public class Maitre {
 /* Class Server */
+    private  ServerSocket serverSocket;
+    private ExecutorService pool;
+
+    public Maitre(int port, int poolSize) {
+        try {
+            serverSocket = new ServerSocket(port);
+            pool = Executors.newFixedThreadPool(poolSize);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void  run() throws IOException {
+        while (true){
+            pool.execute(new Esclave(serverSocket.accept()));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Maitre maitre = new Maitre(3344,5);
+        maitre.run();
+    }
 }
