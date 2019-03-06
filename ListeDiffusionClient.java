9b59921de2fa6775de341c82413978767d5fd1b1
Ameni MTIBAA <ameni.mtibaa17@gmail.com>
2019-03-06 21:43:22 +0100

    Update ListeDiffusionClient.java

--- ListeDiffusionClient.java
+++ ListeDiffusionClient.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This file must be runned in a separet project 
+ * this is the test Client of the server
  */
 package listediffusionclient;
 
