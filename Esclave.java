c51eb5083df7c70b3aee37122cd8679d0b8ac935
Ameni MTIBAA <ameni.mtibaa17@gmail.com>
2019-03-06 21:37:02 +0100

    Update Esclave.java

--- Esclave.java
+++ Esclave.java
@@ -1,41 +1,46 @@
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class Esclave implements  Runnable{
-    private final Socket socket;
 
-    public Esclave(Socket socket) {
+public class Esclave implements Runnable {
+    private final Socket socket;
+    private final Server serveur;
+    
+    Esclave(Socket socket, Server serveur) {
         this.socket = socket;
+        this.serveur = serveur;
     }
 
     @Override
     public void run() {
+        try { 
+            BufferedReader input = new BufferedReader(
+                new InputStreamReader(socket.getInputStream(),
+                            "8859_1"),1024); // flux en lecture
+            StringBuffer sb = new StringBuffer();
+            sb.append(input.readLine());
+            System.out.println(sb);
+            String[] commande = sb.toString().split(" ");
+            switch (commande[0]) {
+                case "create_list": 
+                    System.out.println("im heree!");
+                    createList(commande);
+                    break;
 
-
-        InputStream in = null;
-        try {
-            in = socket.getInputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        int i = 0;
-        while (true){
-            try {
-                if (!((i = in.read())!= -1)) break;
-            } catch (IOException e) {
-                e.printStackTrace();
+                default: ;
             }
-            System.out.write(i);
         }
+        catch (IOException e) {System.out.println(e);}
+        finally {
+            try { if (socket != null) socket.close();}
+            catch (IOException e) {}
+        }    }
 
-        try {
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void createList(String[] commande) {
+        ListeDeDiffusion L=new ListeDeDiffusion(commande[1],commande[2],commande[3],commande[4]);
     }
-
-    /* Classe Thread */
+    
 }
