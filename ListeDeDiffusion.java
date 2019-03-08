f75ef048c61d0c215d985246b5d526c0c5227e43
KADRI Adlane <adlan68@live.fr>
2019-03-08 17:10:36 +0100

    Update ListeDeDiffusion.java

--- ListeDeDiffusion.java
+++ ListeDeDiffusion.java
@@ -1,47 +1,58 @@
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.mail.internet.AddressException;
-
+import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class ListeDeDiffusion {
-
-    /**
-     * @param args the command line arguments
-     */
-    public String nomListe;
-    public theme theme;
-    public enum theme {
-        sociale ("sociale"), 
-        evenement ("evenement"), 
-        reunion ("reunion"),  
+    public enum Theme {
+        sociale ("sociale"),
+        evenement ("evenement"),
+        reunion ("reunion"),
         nouvelles ("nouvelles");
+
         private  String theme;
-        theme(String theme) {
+        Theme(String theme) {
             this.theme=theme;
         }
     }
-    public Personne diffuseur;
+    private String nom;
+    private Theme theme;
+    private Personne diffuseur;
     private String password;
-    private List<Personne> abonnes = new ArrayList();
-     
-    //Getters & Setters
+    private ArrayList<Personne> listAbonnes;
 
-    public String getNomListe() {
-        return nomListe;
+    /* Constructor */
+    public ListeDeDiffusion(String nom, String theme, Personne diffuseur, String password) {
+        this.nom = nom;
+        this.theme = Theme.valueOf(theme);
+        this.diffuseur = diffuseur;
+        this.password = password;
+        this.listAbonnes = new ArrayList<>();
     }
 
-    public void setNomListe(String nomListe) {
-        this.nomListe = nomListe;
+    /* Gestion d'abonnement */
+
+    public boolean addAbonnenment(Personne personne){
+        return listAbonnes.add(personne);
     }
 
-    public String getTheme() {
-        return theme.theme;
+    public boolean removeAbonnenement(Personne personne){
+        return listAbonnes.remove(personne);
     }
 
-    public void setTheme(theme theme) {
+    /* Getters And Setters */
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public void setTheme(Theme theme) {
         this.theme = theme;
     }
 
@@ -61,57 +72,27 @@ public class ListeDeDiffusion {
         this.password = password;
     }
 
-    public  List<Personne> getAbonnes() {
-        return abonnes;
+    public ArrayList<Personne> getListAbonnes() {
+        return listAbonnes;
     }
 
-    public void setAbonnes(List<Personne> abonne) {
-        abonnes = abonne;
+    public void setListAbonnes(ArrayList<Personne> listAbonnes) {
+        this.listAbonnes = listAbonnes;
     }
-    
-    
-    //constructeur
 
-    public ListeDeDiffusion(String nomListe, String th, String mail_diffuseur, String password) {
-        this.nomListe = nomListe;
-        this.theme = theme.valueOf(th);
-        this.password = password;
-        try{
-            this.diffuseur = new Personne(mail_diffuseur) ;
-            System.out.println("liste crée ! nom de la liste: " +this.nomListe +
-                ", theme choisi: "+ this.theme.theme+ ", adresse mail du proprietaire: "
-                    +this.diffuseur.getMailAdress());
-            AllList.add(this); //on garde une liste de toutes les listes qu'on crée
-        } catch(AddressException e){
-            Logger.getLogger(ListeDeDiffusion.class.getName()).log(Level.SEVERE, null, e);
-            System.out.println("liste non créer");}    
-    }
-    
-   
-    
     //ajouter abonne
-    public void addAbonne(String mailabonne) {
-        personne abonne;
-        try {
-            abonne = new personne(mailabonne);
-            ListeDeDiffusion.abonnes.add(abonne);
-        } catch (AddressException ex) {
-            Logger.getLogger(ListeDeDiffusion.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public boolean addAbonne(String mailAbonne) throws AddressException {
+          return   listAbonnes.add(new Personne(mailAbonne));
     }
-    
+
     //supprimer abonne
-    public  boolean removeAbonne(String mailabonne) {
-        for(personne abonne : abonnes) 
-            if (abonne.getMailAdress().equals(mailabonne))
-            {
-                System.out.println("Abonné trouvé !");
-                abonnes.remove(abonne);
+    public  boolean removeAbonne(String mailAbonne) {
+        boolean deleted =  listAbonnes.removeIf(personne->personne.getMail().equals(mailAbonne));
+           
+        if (deleted)
                 System.out.println("Abonné supprimé avec succes");
-                return true;
-            }
-        System.out.println("Abonné n'existe pas");
-        return false;
+            else
+                System.out.println("Abonné n'existe pas");
+        return deleted;
     }
-    
 }
