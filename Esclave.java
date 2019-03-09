1d72cb7edb5a9f1c02a6f7cd22535f4f0d4128f6
KADRI Adlane <adlan68@live.fr>
2019-03-09 04:01:22 +0100

    Update Esclave.java

--- Esclave.java
+++ Esclave.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,144 +6,90 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import javax.mail.*;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import java.util.Optional;
+import java.util.Properties;
 
-public class Esclave implements Runnable {
+public class Esclave implements  Runnable{
     private final Socket socket;
-    private final Server serveur;
-    
-    Esclave(Socket socket, Server serveur) {
+    private  Server server ;
+
+    public Esclave(Socket socket) {
         this.socket = socket;
-        this.serveur = serveur;
     }
 
-    @Override
-    public void run() {
-        try { 
-            BufferedReader input = new BufferedReader(
-                new InputStreamReader(socket.getInputStream(),
-                            "8859_1"),1024); // flux en lecture
-            StringBuffer sb = new StringBuffer();
-            sb.append(input.readLine());
-            System.out.println(sb);
-            String[] commande = sb.toString().split(" ");
-            switch (commande[0]) {
-                case "create_list": 
-                    System.out.println("im heree!");
-                    createList(commande);
-                    break;
-                case "remove_list":
-                    String nom = commande[1];
-                    String pswd = commande[2];
-                    deleteList(nom,pswd);
-                    break;
-                case "subscribe_list":
-                    String liste = commande[1];
-                    String mail = commande[2];
-                    subscribeList(liste,mail);
-                    break;
-                case "unscribe_list":
-                    String Liste = commande[1];
-                    String Mail = commande[2];
-                    unscribeList(Liste,Mail);
-                    break;
-                case "send_email_to_list":
-                    String _Liste = commande[1];
-                    String sender = commande[2];
-                    String Pswd = commande[3];
-                    String subject = commande[4];
-                    String body = commande[5];
-                    sendMail(_Liste,sender,Pswd,subject,body);
-                    break;
-                case "afficher_list":
-                    afficheList();
-                    break;
-                default: ;
-            }
-        }
-        catch (IOException e) {System.out.println(e);}
-        finally {
-            try { if (socket != null) socket.close();}
-            catch (IOException e) {}
-        }    }
+    public Esclave(Socket socket, Server server){
+        this.socket = socket;
+        this.server = server;
 
-    public void createList(String[] commande) {
-        ListeDeDiffusion L=new ListeDeDiffusion(commande[1],commande[2],commande[3],commande[4]);
-        serveur.ManageList(L);
     }
-    
+
+    /* Esclave queries */
+    public boolean createList(String[] commande) throws AddressException {
+        ListeDeDiffusion listeDeDiffusion =
+                new ListeDeDiffusion(commande[1],commande[2],commande[3],commande[4]);
+        return  server.addListeDifusion(listeDeDiffusion);
+    }
+
     public void afficheList(){
-        List<ListeDeDiffusion> AllList = serveur.getAllList();
+        ArrayList<ListeDeDiffusion> AllList = server.getAllList();
         int i =1;
         System.out.println("Il y'a "+ AllList.size()+ " liste de diffusion :");
-        for(ListeDeDiffusion n : AllList)
-            {System.out.println("liste numero: " + i + "\n "
-            + "Nom: "+ n.nomListe + ", theme: "+ n.getTheme()
-            +" , nombre d'abonnées: "+ n.getAbonnes().size());
-            i++;}
-        
+        AllList.stream().forEach(e-> System.out.println(e));
     }
 
-    private void deleteList(String nom, String pswd) {
-        List<ListeDeDiffusion> AllList =serveur.getAllList();
-        boolean deleted = false;
-        searchloop:
-        for(ListeDeDiffusion n : AllList)
-        {
-            if((n.getNomListe().equals(nom)) && (n.getPassword().equals(pswd)))
-            {
-                System.out.println("liste trouvé!");
-                serveur.DeleteList(n);
-                System.out.println("liste supprimé avec succés!");
-                afficheList();
-                deleted=true;
-                break searchloop;
-            }     
-        }
-        if(!deleted) System.out.println("nom de liste ou mdp incorrecte!");   
+    private boolean deleteList(String nomlist, String pswd) {
+        ArrayList<ListeDeDiffusion> allList = server.getAllList();
+        boolean deleted =
+                allList.removeIf(l->l.getPassword().equals(pswd) && l.getNom().equals(nomlist));
+        if(deleted){
+            System.out.println("liste supprimé avec succés!");
+            this.afficheList();
+        }else
+            System.out.println("nom de liste ou mdp incorrecte!");
+
+        return deleted;
     }
-    
-    private void subscribeList(String liste, String mail) {
-        List<ListeDeDiffusion> AllList = serveur.getAllList();
-        searchloop:
-        for(ListeDeDiffusion n : AllList)
-        {
-            if(n.getNomListe().equals(liste))
-            {
-               n.addAbonne(mail);
-               System.out.println("vous avez été ajouté !");
-                
+
+
+    private boolean subscribeList(String liste, String mail) throws AddressException {
+        ArrayList<ListeDeDiffusion> allList = server.getAllList();
+        boolean added = false ;
+        for(ListeDeDiffusion list : allList){
+            if(list.getNom().equals(liste)){
+                added = list.addAbonne(mail);
+                System.out.println("vous avez été ajouté !");
+                break ;
             }
         }
+        return added;
     }
-    
-    private void unscribeList(String Liste, String Mail) {
-        List<ListeDeDiffusion> AllList = serveur.getAllList();
-        searchloop:
-        for(ListeDeDiffusion n : AllList)
-        {
-            if(n.getNomListe().equals(Liste))
-            {n.removeAbonne(Mail);
-            System.out.println("vous êtes désabonné de la liste de diffusion: "+Liste);
-            }
-        }
+
+    private boolean unscribeList(String liste, String Mail) {
+        ArrayList<ListeDeDiffusion> allList = server.getAllList();
+        boolean deleted = allList.removeIf(list->list.getNom().equals(liste));
+        if (deleted)
+            System.out.println("vous êtes désabonné de la liste de diffusion: "+liste);
+        return deleted;
     }
-    
-    private ListeDeDiffusion GetList(String nomListe){
-    List<ListeDeDiffusion> AllList = serveur.getAllList();
-        for(ListeDeDiffusion n : AllList)
-        {
-            if(n.getNomListe().equals(nomListe))
-            return n;
+
+
+    private ListeDeDiffusion GetListeDeDiffusion(String nomListe) {
+
+        Optional<ListeDeDiffusion> list =
+                server.getAllList().stream().filter(l -> l.getNom().equals(nomListe)).findFirst();
+        if (list.isPresent()){
+            ListeDeDiffusion listeDeDiffusion = list.get();
+            return listeDeDiffusion;
         }
         return null;
     }
-    
-    private void sendMail(String NameList, String from, String password, String MailSubject, String MailBody)
-    {
-        ListeDeDiffusion liste = GetList(NameList);
+
+    private void sendMail(String NameList, String from, String password, String MailSubject, String MailBody){
+        ListeDeDiffusion liste = GetListeDeDiffusion(NameList);
         if (liste ==null)
         {
             System.out.println("liste de diffusion n'existe pas!!");
@@ -152,38 +97,108 @@ public class Esclave implements Runnable {
         }
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
-	props.put("mail.smtp.starttls.enable", "true");
-	props.put("mail.smtp.host", "smtp.gmail.com");
-	props.put("mail.smtp.port", "587");     
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
         Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(from, password);
-			}
-		  });
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(from, password);
+                    }
+                });
         try {
-         // Create a default MimeMessage object.
-         MimeMessage message = new MimeMessage(session);
-         message.setFrom(new InternetAddress(from));
-         List<personne> Abonnes = liste.getAbonnes();
-         List<String> to =new ArrayList();
-         String receivers;
-         for(personne p : Abonnes)
-         {
-             to.add(p.getMailAdress());
-         }
-         receivers = String.join(",", to);
-         InternetAddress[] parse = InternetAddress.parse(receivers , true);
-         message.setRecipients(javax.mail.Message.RecipientType.TO,  parse);
-         // Set Subject: header field
-         message.setSubject(MailSubject);
-         // Now set the actual message
-         message.setText(MailBody);
-         // Send message
-         Transport.send(message);
-         System.out.println("Email envoyé.");
-      } catch (MessagingException mex) {
-         mex.printStackTrace();
-      }
-   }        
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            List<Personne> Abonnes = liste.getListAbonnes();
+            List<String> to =new ArrayList();
+            String receivers;
+            for(Personne p : Abonnes)
+            {
+                to.add(p.getMail());
+            }
+            receivers = String.join(",", to);
+            InternetAddress[] parse = InternetAddress.parse(receivers , true);
+            message.setRecipients(javax.mail.Message.RecipientType.TO,  parse);
+            // Set Subject: header field
+            message.setSubject(MailSubject);
+            // Now set the actual message
+            message.setText(MailBody);
+            // Send message
+            Transport.send(message);
+            System.out.println("Email envoyé.");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void run() {
+        BufferedReader input = null; // flux en lecture
+        try {
+            input = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(),
+                            "8859_1"),1024);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        StringBuffer sb = new StringBuffer();
+
+        try {
+            sb.append(input.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sb);
+
+        String[] commande = sb.toString().split(" ");
+        try {
+            switch (commande[0]) {
+            case "create_list":
+                try {
+                    createList(commande);
+                } catch (AddressException e1) {
+                    e1.printStackTrace();
+                }
+                break;
+            case "remove_list":
+                String nom = commande[1];
+                String pswd = commande[2];
+                deleteList(nom,pswd);
+                break;
+            case "subscribe_list":
+                String liste = commande[1];
+                String mail = commande[2];
+                try {
+                    subscribeList(liste,mail);
+                } catch (AddressException e1) {
+                    e1.printStackTrace();
+                }
+                break;
+            case "unscribe_list":
+                String Liste = commande[1];
+                String Mail = commande[2];
+                unscribeList(Liste,Mail);
+                break;
+            case "send_email_to_list":
+                String _Liste = commande[1];
+                String sender = commande[2];
+                String Pswd = commande[3];
+                String subject = commande[4];
+                String body = commande[5];
+                sendMail(_Liste,sender,Pswd,subject,body);
+                break;
+            case "afficher_list":
+                afficheList();
+                break;
+            default: ;
+        }
+    } finally {
+        try { if (socket != null) socket.close();}
+        catch (IOException e) {}
+    }
+}
+
 }
