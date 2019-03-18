52491f6d94448e5844b5b053ead7b50bc9f920c2
Ayyoub <46877105+Krich75@users.noreply.github.com>
2019-03-18 17:50:45 +0100

    Use this file to validate XML file structure
    it returns FALSE for Adlan's file .. so we have to correct that.

---
+++ XmlValidator.java
@@ -0,0 +1,45 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+public class XmlValidator {
+    public static final String XML_FILE = "XmlFile.xml";
+    public static final String SCHEMA_FILE = "XsdFile.xsd";
+
+    public static void main(String[] args) {
+        XmlValidator XMLValidator = new XmlValidator();
+        boolean valid = XMLValidator.validate(XML_FILE, SCHEMA_FILE);
+
+        System.out.printf("%s validation = %b.", XML_FILE, valid);
+    }
+
+    private boolean validate(String xmlFile, String schemaFile) {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        try {
+            Schema schema = schemaFactory.newSchema(new File(getResource(schemaFile)));
+
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File(getResource(xmlFile))));
+            return true;
+        } catch (SAXException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private String getResource(String filename) throws FileNotFoundException {
+        URL resource = getClass().getClassLoader().getResource(filename);
+        Objects.requireNonNull(resource);
+
+        return resource.getFile();
+    }
+}
