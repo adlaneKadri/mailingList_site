002b62d87e642181aa146f8dc24451d4c9956db3
Ayyoub <46877105+Krich75@users.noreply.github.com>
2019-03-25 15:38:44 +0100

    Update Serverssl.java

--- Serverssl.java
+++ Serverssl.java
@@ -18,10 +18,10 @@ import java.util.logging.Logger;
  *
  * @author KRICH
  */
-public class Server {
-    private SSLServerSocket serverSocket;
-    private SSLSocket socket;
-    public static final int port = 35786;
+public class Serverssl {
+    private SSLServerSocket sslServerSocket;
+    private SSLSocket sslSocket;
+    public static final int port = 33333;
     private static final int poolSize = 10;
     private ExecutorService pool = null;
     private Boolean isFinished = false;
@@ -29,56 +29,30 @@ public class Server {
 
 
     //constructor
-    public Server(int port, int size) {
-	       Security.addProvider(new Provider());
-        //specifing the keystore file which contains the certificate/public key and the private key
-        System.setProperty("javax.net.ssl.keyStore","myKeyStore.jks");
-        //specifing the password of the keystore file
-        System.setProperty("javax.net.ssl.keyStorePassword","123456");
-        //This optional and it is just to show the dump of the details of the handshake process 
-        System.setProperty("javax.net.debug","all");       
+    public Serverssl(int port, int size) {
+	
+            AllList = new ArrayList();
 
-	 try {
 
-		   //SSLServerSocketFactory establishes the ssl context and and creates SSLServerSocket 
-            SSLServerSocketFactory sslServerSocketfactory = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
-            //Create SSLServerSocket using SSLServerSocketFactory established ssl context
-            SSLServerSocket serverSocket = (SSLServerSocket)sslServerSocketfactory.createServerSocket(port,size);
-            System.out.println("Echo Server Started & Ready to accept Client Connection");
-            //Wait for the SSL client to connect to this server
-		socket = (SSLSocket)serverSocket.accept();		
-
-            //Create InputStream to recive messages send by the client
-        //    DataInputStream inputStream = new DataInputStream(sslSocket.getInputStream());
-            //Create OutputStream to send message to client
-          //  DataOutputStream outputStream = new DataOutputStream(sslSocket.getOutputStream());
-          //  outputStream.writeUTF("Hello Client, Say Something!");
-            //Keep sending the client the message you recive unless he sends the word "close"
-		
-            pool = Executors.newFixedThreadPool(poolSize);
-        } catch (IOException ex) {
-            Logger.getLogger(Server.class.
-                    getName()).log(Level.SEVERE, null, ex);
-        }
 
     }
 
 
     //Getters&setters
     public ServerSocket getServerSocket() {
-        return serverSocket;
+        return sslServerSocket;
     }
 
-    public void setServerSocket(SSLServerSocket serverSocket) {
-        this.serverSocket = serverSocket;
+    public void setServerSocket(SSLServerSocket sslServerSocket) {
+        this.sslServerSocket = sslServerSocket;
     }
 
     public Socket getSocket() {
-        return socket;
+        return sslSocket;
     }
 
-    public void setSocket(SSLSocket socket) {
-        this.socket = socket;
+    public void setSocket(SSLSocket sslSocket) {
+        this.sslSocket = sslSocket;
     }
 
     public ExecutorService getPool() {
@@ -107,18 +81,104 @@ public class Server {
 
 
     public void ManageRequest(){
+
+
+	 try
+
+	{
+
+            Security.addProvider(new Provider());
+            //specifing the keystore file which contains the certificate/public key and the private key
+            System.setProperty("javax.net.ssl.keyStore","myKeyStore.jks");
+            //specifing the password of the keystore file
+            System.setProperty("javax.net.ssl.keyStorePassword","123456");
+            //This optional and it is just to show the dump of the details of the handshake process
+            System.setProperty("javax.net.debug","all");
+            SSLServerSocketFactory sslServerSocketFactory = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+            SSLServerSocket sslServerSocket = (SSLServerSocket)sslServerSocketFactory.createServerSocket(port);
+
+            System.out.println("je suis la pt con !! ");
+          //  sslSocket = (SSLSocket)sslServerSocket.accept();
+            //Create InputStream to recive messages send by the client
+          //  DataInputStream inputStream = new DataInputStream(sslSocket.getInputStream());
+            //Create OutputStream to send message to client
+          //  DataOutputStream outputStream = new DataOutputStream(sslSocket.getOutputStream());
+
+            //outputStream.writeUTF("je suis la pt con !!! ");
+            //Keep sending the client the message you recive unless he sends the word "close"
+
+            Esclavessl es = new Esclavessl( (SSLSocket)sslServerSocket.accept(),this);
+            Thread tt = new Thread(es);
+            tt.start();
+          }  catch(Exception ex)  {
+                System.err.println("Error Happened : "+ex.toString());
+            }
+            /*
+
+            while(true)
+            {
+                String recivedMessage = inputStream.readUTF();
+                System.out.println("Client Said : " + recivedMessage);
+                if(recivedMessage.equals("close"))
+                {
+                    outputStream.writeUTF("Bye");
+                    outputStream.close();
+                    inputStream.close();
+                    sslSocket.close();
+                    sslServerSocket.close();
+                    break;
+                }
+                else
+                {
+                    outputStream.writeUTF("You Said : "+recivedMessage);
+                }
+            }
+        }*/
+
+
+    }
+
+
+
+	/*   while(true)
+            {
+                String recivedMessage = inputStream.readUTF();
+                System.out.println("Client Said : " + recivedMessage);
+                if(recivedMessage.equals("close"))
+                {
+                    outputStream.writeUTF("Bye");
+                    outputStream.close();
+                    inputStream.close();
+                    sslSocket.close();
+                    sslServerSocket.close();
+                    break;
+                }
+                else
+                {
+                    outputStream.writeUTF("You Said : "+recivedMessage);
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            System.err.println("Error Happened : "+ex.toString());
+        }*/
+
+
+
+
+	/*
         while (!isFinished) {
             try {
-                pool.execute(new Esclavessl((SSLSocket)serverSocket.accept(),this));
+
+                pool.execute(new Esclavessl(serverSocket.accept(),this));
             }
             catch (IOException e) {System.out.println(e);}
             finally {
                 try { if (socket != null) socket.close();}
                 catch (IOException e) {}
             }
-        }
-    }
-
+        }*/
     public void ManageList(ListeDeDiffusion l)
     {
         AllList.add(l);
